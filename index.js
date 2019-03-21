aef930f2140ceb8a0694326182b0fe19ce0e538f
MiguelZN <miguelzn>
2019-03-21 01:48:16 -0400

    base_game_finished

--- index.js
+++ index.js
@@ -6,6 +6,139 @@ var server = app.listen(3000, function(req,res){
 	console.log('server online');
 });
 
+
+
+//Game Variables
+var num_rows = 3;
+var num_columns = 3;
+var board = createBoard(num_rows, num_columns);
+var game_turn = decideTurn(); //who's turn it is within the game
+var players = {}; //the dictionary of players
+var player_count = 0; //player counter to determine who is on team player1, team player2
+console.log(game_turn);
+
+//Score Variables
+var player1_score = 0;
+var player2_score = 0;
+var ties = 0;
+
+function createBoard(num_rows, num_columns){
+	var board = [];
+
+	for(var r =0;r<num_rows;r++){
+		var row = [];
+		for(var c=0;c<num_columns;c++){
+			var column = "_";
+
+			row.push(column);
+
+		}
+
+		board.push(row);
+	}
+
+	console.log(board);
+	console.log(board[0][0]);
+
+	return board;
+}
+
+function setPiece(board, player, row, column){
+	if(player.player_turn=="X"){
+		board[row][column] = "X";
+	}
+	else if(player.player_turn == "O"){
+		board[row][column] = "O";
+	}
+}
+
+
+function assignPlayer(socket, player_count){
+	let socket_id = socket.id;
+	console.log(socket_id in players);
+
+	if(socket_id in players == false){
+		console.log("SOCKET ID NOT FOUND IN PLAYERS");
+		let player_piece = player_count%2;
+		let id = socket.id;
+		console.log(id);
+		console.log(player_piece);
+		console.log(Object.keys(players));
+
+		if(player_piece==0){
+			player_piece = "X";
+		}
+		else{
+			player_piece = "O";
+		}
+
+		console.log("ASSIGNED PLAYER");
+
+		let new_player = {
+			id: id,
+			piece: player_piece //either "X" or "Y"
+		};
+
+		players[id] = new_player;
+
+		io.to(socket_id).emit('game_info', 
+		{
+			player:new_player,
+			num_rows: num_rows,
+			num_columns: num_columns,
+			game_turn : game_turn
+		});
+	}
+	else{
+		console.log("SOCKET ID WAS ALREADY IN PLAYERS");
+
+		let existing_player = players[socket_id];
+		io.to(socket_id).emit('game_info',{
+			player: existing_player,
+			num_rows:num_rows,
+			num_columns: num_columns,
+			game_turn: game_turn
+		});
+	}
+}
+
+function switchTurn(gameturn){
+	console.log("SWITCHED TURN");
+	if(gameturn == "X"){
+		gameturn = "O";
+	}
+	else{
+		gameturn = "X";
+	}
+
+	return gameturn;
+}
+
+function decideTurn(){
+	var choice = Math.floor(Math.random()*2)+1;
+	var turn;
+
+	console.log("DECIDED TURN");
+
+	switch(choice){
+		case 1:
+		turn = "X";
+		break;
+
+		case 2:
+		turn = "O";
+		break;
+
+		default:
+		console.log("DID NOT WORK");
+		break;
+	}
+
+	console.log(turn);
+	return turn;
+}
+
+
 //Static Files
 app.use(express.static('public'));
 
@@ -18,14 +151,235 @@ app.get('/',function(req,res){
 //Socket Setup
 var io = socket(server);
 
-io.on('connection',function(socket){
-	console.log('User '+socket.id+' has logged on.');
+setInterval(function(){
+	//Checks/Updates the game every 100ms 
+	updateGame(board)
+	checkIfWinner(board,num_rows,num_columns);
 
-	socket.on('playclick',function(){
-		console.log('User '+socket.id+' has entered the game.');
+}, 100);
+
+//checks the inputted board for if there is a winner or no winner
+function checkIfWinner(board, num_rows, num_columns){
+	isWinner = false; //no winner
+
+	amount_required = num_columns;//the number of symbol occuring in row/column/diagonal to win
+	amount_X_row = 0;
+	amount_X_column = 0;
+	amount_X_diagonal = 0;
+
+	amount_O_row = 0;
+	amount_O_column = 0;
+	amount_O_diagonal = 0;
+
+
+	//Checks Rows and Columns
+	for(var r = 0;r<num_rows;r++){
+		for(var c = 0;c<num_columns;c++){
+			//Checks the rows 
+			if(board[r][c]=="X"){
+				amount_X_row+=1;
+			}
+
+			if(board[r][c]=="O"){
+				amount_O_row+=1;
+			}
+
+			//Checks Columns
+			if(board[c][r]=="X"){
+				amount_X_column+=1;
+			}
+
+			if(board[c][r]=="O"){
+				amount_O_column+=1;
+			}
+		}
+
+		//ROWS:
+		//if the amount of X's found did not meet the amount required to win then reset
+		if(amount_X_row<amount_required){
+			amount_X_row = 0;
+		}
+		else if(amount_X_row>=amount_required){
+			isWinner = "X"; //says the winner is X
+		}
+		//if the amount of O's found did not meet the amount required to win then reset
+		if(amount_O_row<amount_required){
+			amount_O_row = 0;
+		}
+		else if(amount_O_row>=amount_required){
+			isWinner = "O";
+		}
+
+		//COLUMNS:
+		if(amount_X_column<amount_required){
+			amount_X_column = 0;
+		}
+		else if(amount_X_column>=amount_required){
+			isWinner = "X"; //says the winner is X
+		}
+		//if the amount of O's found did not meet the amount required to win then reset
+		if(amount_O_column<amount_required){
+			amount_O_column = 0;
+		}
+		else if(amount_O_column>=amount_required){
+			isWinner = "O";
+		}
+	}
+
+	//LEFT TO RIGHT BOTTOM DIAGONALLY
+	for(var c=0;c<num_columns;c++){
+		var r = c; //starting at the 0 row
+
+		console.log("r:"+r);
+		console.log("c:"+c);
+		if(board[r][c]=="X"){
+			amount_X_diagonal+=1;
+		}
+
+		if(board[r][c]=="O"){
+			amount_O_diagonal+=1;
+		}
+	}
+
+	//Checks X diagonally left to bottom
+	if(amount_X_diagonal < amount_required){
+		amount_X_diagonal = 0;
+	}
+	else if(amount_X_diagonal>=amount_required){
+		isWinner = "X";
+	}
+
+	//Checks O diagonally left to bottom
+	if(amount_O_diagonal < amount_required){
+		amount_O_diagonal = 0;
+	}
+	else if(amount_O_diagonal>=amount_required){
+		isWinner = "O";
+	}
+
+	console.log("X DIAG:"+amount_X_diagonal);
+	console.log("O DIAG:"+amount_O_diagonal);
+
+	//RIGHT TO LEFT BOTTOM DIAGONALLY
+	var r = 0; //temporary row to start at top left and increment 1 each time to get to bottom right
+	for(var c=num_columns-1;c>=0;c--){
+		if(board[r][c]=="X"){
+			amount_X_diagonal+=1;
+		}
+
+		if(board[r][c]=="O"){
+			amount_O_diagonal+=1;
+		}
+
+		r+=1;
+	}
+	r=0;
+
+	//Checks X diagonally left to bottom
+	if(amount_X_diagonal < amount_required){
+		amount_X_diagonal = 0;
+	}
+	else if(amount_X_diagonal>=amount_required){
+		isWinner = "X";
+	}
+
+
+	//Checks O diagonally left to bottom
+	if(amount_O_diagonal < amount_required){
+		amount_O_diagonal = 0;
+	}
+	else if(amount_O_diagonal>=amount_required){
+		isWinner = "O";
+	}
+
+
+	//Checks if there actually is a winner. 
+	//If there is a winner, then server notifies the clients connected of which team
+	//has won and refreshes the board
+	if(isWinner!=false){
+		//Sends to clients who was the winner
+		io.emit('winner', {
+			winner:isWinner
+		});
+		resetBoard();
+
+		return;
+	}
+
+	//Checks if the board is full and there are no more empty spots
+	var empty_spots_left = num_rows*num_columns;
+	for(var r=0;r<num_rows;r++){
+		for(var c=0;c<num_columns;c++){
+			if(board[r][c]=="X" || board[r][c]=="O"){
+				empty_spots_left-=1;
+			}
+		}
+	}
+
+	if(empty_spots_left==0){
+		resetBoard();
+		io.emit('no_winner');
+		return;
+	}
+}
+
+function resetBoard(){
+	board = createBoard(num_rows, num_columns);
+}
+
+function updateGame(board){
+	// console.log("updating clients' games");
+	// console.log(board);
+
+	io.emit('update_game', {
+		board: board,
+		game_turn: game_turn,
+	});
+
+	console.log(players);
+	console.log(Object.keys(players).length);
+}
+
+io.on('connection',function(socket){
+	player_count++;
+	console.log('User '+socket.id+' has logged on.');
+	let socket_id = socket.id;
+	let current_player = players[socket_id]; //pulls out the client who is connecting 's player'
+
+	let keys = Object.keys(players);
+	console.log(socket_id);
+	console.log(players);
+	console.log(players[keys[0]]);
+	console.log(current_player);
+
+	//sending to client the player when they connect
+	assignPlayer(socket,player_count);
+
+
+
+	socket.on('button_pressed', function(data){
+		console.log("USER:"+socket.id+ " PRESSED BUTTON AT ROW:"+data.row+", COLUMN:"+data.column);
+		console.log(players);
+
+		console.log(data);
+		let socket_id = socket.id;
+		let piece = players[socket_id].piece; //retrieves the player piece 
+
+		console.log("OLD PLAYER PIECE:"+piece);
+		board[data.row][data.column] = piece;
+		console.log("OLDTURN:"+game_turn);
+		game_turn = switchTurn(game_turn); //switches game turn EX: current_turn == 'X' to current_turn == "o"
+		console.log("NEWTURN:"+game_turn);
+
+		console.log("NEW PLAYER PIECE:"+players[socket_id].piece);
 	})
 
 	socket.on('disconnect',function(){
 		console.log('User '+socket.id+' has logged off.');
+		let socket_id = socket.id;
+
+		delete players[socket_id]; //removes the player from the dictionary of players
 	})
+
+
 });
--- public/client.js
+++ public/client.js
@@ -1,31 +1,191 @@
-
-// button1 = document.getElementById('button1');
-
-// button1.addEventListener('click',function(){
-// 	console.log(firebase.database());
-// })
+var SOCKET_ID;
+var socket = io.connect('http://localhost:3000');
 
 var hold_game = document.getElementById("holdgame"); //div that will hold the tictactoe game
-
+var current_turn_label = document.getElementById('current_turn_label'); //pulls game_state label
+var team_label = document.getElementById('team_label');
 
 //TICTACTOE GAME
-var rows= 3;
-var columns = 3;
+var num_rows;
+var num_columns;
+var box_width;
+var box_height;
+var client_player;
 
-var box_width = 1/rows;
-var box_height = 1/columns;
+//Updated variables
+var board;
+var game_turn;
+
+socket.on('connect',function(){
+	console.log("CONNECTED");
+	SOCKET_ID = socket.io.engine.id; //gives the variable 'SOCKET_ID' to the client, the socket 
+									//connection unique id (to idenify client-speciifc player)
+
+	updateBackgroundColor(1500);
+});
+
+socket.on('game_info',function(data){
+	console.log("GAME INFO");
+	client_player = data.player; //sets 'client_player' variable to the client's player from the server
+	// console.log("CLIENT_PLAYER:"+client_player);
+	console.log("CLIENT PLAYER BELOW:");
+	console.log(client_player);
+
+	num_rows = data.num_rows;
+	num_columns = data.num_columns;
+	box_width = Math.floor((1/num_rows)*100)-1.5;
+	box_height = Math.floor((1/num_columns)*100)-2;
+
+	game_turn = data.game_turn;
+
+	//MAIN:----------------------------
+	createBoard(num_rows,num_columns);
 
 
-for(var r =0;r<rows;r++){
-	for(var c=0;c<columns;c++){
-		let box = createElement('div');
-		box.id = 'box'+r+c;
-		console.log(box.id);
+	updateTeamLabel();
+});
+
+socket.on('update_game',function(data){
+	board = data.board;
+	game_turn = data.game_turn;
+	redrawBoard(data.board,num_rows,num_columns);
+	updateCurrentTurnLabel();
+})
+
+socket.on('winner',function(data){
+	let winner = data.winner;
+	alert("Team "+winner+" has won! New game proceeding...");
+})
+
+socket.on('no_winner',function(){
+	alert("No winner! New game proceeding...");
+})
+
+
+function updateBackgroundColor(interval_speed){
+	setInterval(function(){
+		console.log(client_player);
+		setElementRandomColor(document.documentElement);
+		interval_speed = Math.floor(Math.random()*500);
+	},interval_speed);
+}
+
+
+function clickedButton(id, row, column){
+	let element = document.getElementById(id);
+	console.log(element);
+	let color = element.style.backgroundColor;
+	let color_darken_amount = 50;
+
+	let rgbstr = color.substring(0,3); //only pulls substring [beg,end) 
+	let color1 = color.substring(4,7);
+	let color2 = color.substring(8,12);
+	let color3 = color.substring(13,17);
+
+	let pressedcolor = rgbstr+"("+(parseInt(color1)-color_darken_amount)+","+(parseInt(color2)-color_darken_amount)+","+(parseInt(color3)-color_darken_amount)+")";
+	element.style.backgroundColor = pressedcolor;
+
+	console.log("PRESSEDCOLOR:"+pressedcolor);
+	console.log("ROW:"+row + ", COLUMN:"+column);
+
+	setTimeout(function(){
+		element.style.backgroundColor = color;
+	},50);
+
+	// console.log(client_player.piece);
+	// console.log(game_piece);
+	// console.log(this.board);
+
+
+	//Detects which button the user pressed and tells the server that the User has pressed the button at
+	//row: 'row', column: 'column'
+	let selected_piece = board[row][column];
+	console.log(selected_piece);
+
+	console.log("CURRENT TURN:"+ game_turn);
+	console.log("PLAYER's PIECE:"+client_player.piece);
+	console.log("SOCKET_ID:"+client_player.id);
+	if(game_turn == client_player.piece){
+
+	//if the piece does not equal to player's piece (TEAM PIECE)
+	if(selected_piece != "X" && selected_piece != "O"){
+		//Notifies the server that the user pressed the button located on row: 'rownumber' column: 'columnnumber'
+		console.log("PRESSED BUTTON");
+		socket.emit('button_pressed',{
+			row: row,
+			column: column
+		})
 	}
+	//
+	else if(selected_piece==client_player.piece){
+		console.log("SELECTED THE SAME TILE PICK AGAIN");
+		alert("You have selected the same piece tile! Pick again.");
+	}
+	else if(selected_piece != client_player.piece){
+		alert("Another team's piece is already there! Pick again.");
+	}
+}
+}
+
+//Updates the player's team to what their piece is: either "TEAM X" or "TEAM O"
+function updateTeamLabel(){
+	team_label.innerHTML = "TEAM:"+client_player.piece;
+}
+
+function updateCurrentTurnLabel(){
+	current_turn_label.innerHTML = "Current Turn:"+game_turn;
+}
+
+function createBoard(num_rows, num_columns){
+	for(let r =0;r<num_rows;r++){
+		for(let c=0;c<num_columns;c++){
+			let box = document.createElement('div');
+			let id = 'box'+"r"+r+"c"+c;
+			box.id = id;
+			console.log(box.id);
+			let css = "background-color:rgb(150,150,150);width:"+box_width+"%;"+"height:"+box_width+"%;"+"display:inline-block;"+"border:solid;"+"margin-left:1px;";
+			console.log(css);
+			box.style.cssText = css;
+			hold_game.appendChild(box);
+			box.onclick = function(){clickedButton(id, r, c)};
+
+
+			//Adding font 
+			box.className = "CENTER";
+			let paragraph = document.createElement('p');
+			paragraph.className = "CENTER SQUARE";
+			let text = document.createTextNode("_");
+			paragraph.id = 'p'+"r"+r+"c"+c;
+
+			//Adding paragraph into each tictactoe box
+			paragraph.appendChild(text);
+			box.appendChild(paragraph);
+
+			//
+		}}
+}
+
+function redrawBoard(board,num_rows, num_columns){
+	for(let r =0;r<num_rows;r++){
+		for(let c=0;c<num_columns;c++){
+			let current_text = document.getElementById('p'+"r"+r+"c"+c); //pulls each html box and allows to edit it
+			current_text.innerHTML = board[r][c]; //updates the text of each box's paragraph's text node
+			// console.log(board);
+			// console.log(current_text);
+	}}
+}
+
+function setElementRandomColor(element){
+	let color1 = Math.floor(Math.random()*255);
+	let color2 = Math.floor(Math.random()*255);
+	let color3 = Math.floor(Math.random()*255);
+
+	element.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")";
 }
 
 
 
-//box.onclick = function(){}
-//box.innerHTML = "hello";
-//box.style.cssText = "position:absolute;top:300px";
+
+
+
+
